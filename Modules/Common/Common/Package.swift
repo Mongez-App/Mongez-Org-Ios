a52ba594fd8ba701844c93c078863bcd90ee067c
Mazen Amr <139044003+mazen675@users.noreply.github.com>
2026-08-15 17:16:27 +0300

    chore : add DI and imported Common

--- Modules/Common/Common/Package.swift
+++ Modules/Common/Common/Package.swift
@@ -5,19 +5,25 @@ import PackageDescription
 
 let package = Package(
     name: "Common",
+    platforms: [.iOS(.v16)],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "Common",
             targets: ["Common"]),
     ],
+    dependencies: [
+        .package(url: "https://github.com/Swinject/Swinject.git", from: "2.8.3")
+    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "Common"),
-        .testTarget(
-            name: "CommonTests",
-            dependencies: ["Common"]),
+            name: "Common",
+            dependencies: [
+                .product(name: "Swinject-Dynamic", package: "Swinject")
+            ],
+            resources: [.process("Themes/Assets.xcassets")]
+        ),
     ]
 )
